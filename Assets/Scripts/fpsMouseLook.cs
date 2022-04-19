98cd8b6df302c3608a624c37aa4d05b8a06f45e8
anarchy-artichokey <75590673+anarchy-artichokey@users.noreply.github.com>
2022-04-19 18:41:05 -0500

    adding the mouse look script for the fps

--- Assets/Scripts/fpsMouseLook.cs
+++ Assets/Scripts/fpsMouseLook.cs
@@ -7,12 +7,16 @@ public class fpsMouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 euler = transform.rotation.eulerAngles;
+        x = euler.x;
+        y = euler.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x += Input.GetAxis("Mouse Y") * Time.deltaTime;
+        y += Input.GetAxis("Mouse X") *  Time.deltaTime;
+        x = Mathf.Clamp(x, -maxVerticalAngle, maxVerticalAngle);
     }
 }
